a4b94b99fbaf8196fd48db629505087bd713fafa
fuma-afk <59653552+fuma-afk@users.noreply.github.com>
2024-12-30 18:30:56 +0100

    Create vars.nix

---
+++ home-manager/modules/hyprland/vars.nix
@@ -0,0 +1,19 @@
+{ inputs, ... }:
+{
+  wayland.windowManager.hyprland = {
+    enable = true;
+    settings = {
+#      "$internalM" = "DP-1";
+#      "$externalM" = "HDMI-A-1";
+#      "$keyboardLayout" = "fr";
+      "$scripts" = "/home/fuma/nix/scripts";
+
+#      monitor = [
+#        "$internalM, 2560x1440@165, 0x0, 1"
+#        "$externalM, 2560x1440@60, -2560x0, 1"
+#      ];
+
+    };
+  };
+
+}
